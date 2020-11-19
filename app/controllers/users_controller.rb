886e3962b463c58876e67916154c1fe025ce2003
dani7beth <danielledona28@gmail.com>
2020-11-19 12:00:49 -0700

    added some minor sass

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,30 @@
 class UsersController < ApplicationController
   def index
+    @users = User.all
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   def new
+    @user = User.new
   end
+
+  def create
+    @user = User.new(user_params)
+    @user.save ? redirect_to @user : render :new #if it sucessfully saves true else false
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to users_path
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:first_name, :last_name)
+  end
+
 end
